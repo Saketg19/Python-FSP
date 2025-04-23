bc528ffa75e8a4c80e0b1e8ee013f020e608d093
Saketg19 <123272261+Saketg19@users.noreply.github.com>
2025-04-23 10:59:39 +0530

    Add files via upload

---
+++ InsertAndRemove.py
@@ -0,0 +1,12 @@
+a=[1,2,3,4,5]
+print(a)
+#for x in range(5):
+   # x=int(input())
+  #  a.append(x)
+#print(a)
+a.extend([1.3,1.5,1.7])
+print(a)
+a.insert(1,"Aman")
+print(a)
+a.remove("Aman")
+print(a)
